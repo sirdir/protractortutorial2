51ad102458980818187d4dd46d551673dc17a32b
sirdir <phddir@gmail.com>
2017-06-19 19:34:16 +0300

    step 2

--- spec.js
+++ spec.js
@@ -1,14 +1,33 @@
 describe('Protractor Demo App', function() {
     var url = 'http://juliemr.github.io/protractor-demo/';
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var goButton = $('#gobutton');
+    var latestResult = element(by.binding('latest'));
+
+    beforeEach(function() {
+        browser.get(url);
+    });
+
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
 
     it('should add one and two', function() {
-        browser.get(url);
-        element(by.model('first')).sendKeys(1);
-        element(by.model('second')).sendKeys(2);
+        firstNumber.sendKeys(1);
+        secondNumber.sendKeys(2);
 
-        $('#gobutton').click();
+        goButton.click();
 
-        expect(element(by.binding('latest')).getText()).
-        toEqual('3'); // This is wrong!
+        expect(latestResult.getText()).toEqual('3');
+    });
+
+    it('should add four and six', function() {
+        firstNumber.sendKeys(4);
+        secondNumber.sendKeys(6);
+
+        goButton.click();
+
+        expect(latestResult.getText()).toEqual('10');
     });
 });

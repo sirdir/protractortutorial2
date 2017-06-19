e9551a77c0afb5633c13fc8177bc067d2c1f21a7
sirdir <phddir@gmail.com>
2017-06-19 15:25:15 +0300

    chenge locators in tutorial to $ where css was used

--- todo-spec.js
+++ todo-spec.js
@@ -3,15 +3,16 @@ describe('angularjs homepage todo list', function() {
         browser.get('https://angularjs.org');
 
         element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-        element(by.css('[value="add"]')).click();
+        // element(by.css('[value="add"]')).click();
+        $('[value="add"]').click();
 
         var todoList = element.all(by.repeater('todo in todoList.todos'));
         expect(todoList.count()).toEqual(3);
         expect(todoList.get(2).getText()).toEqual('write first protractor test');
 
         // You wrote your first test, cross it off the list
-        todoList.get(2).element(by.css('input')).click();
-        var completedAmount = element.all(by.css('.done-true'));
+        todoList.get(2).$('input').click();
+        var completedAmount = $$('.done-true');
         expect(completedAmount.count()).toEqual(2);
     });
 });
